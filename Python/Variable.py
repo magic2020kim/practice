2aaed28d00b69d512387812a41da6eab25c301ba
magic2020kim <jamesloo003@gmail.com>
2025-01-12 01:20:04 +0800

    Lecture 2: Variable
    Study variable type and implement via printing a statement and bring in the variable data.

---
+++ Python/Variable.py
@@ -0,0 +1,10 @@
+name = 'John'   #character
+age = 20        #integer
+height = 175.5  #float
+is_new = True   #boolean
+
+print('This patient is ', name, 'and his age is', age, 'with height =' ,height, 'status is_new =', is_new)
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_new))
